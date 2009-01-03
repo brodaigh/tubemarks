235f334b8835c208ecadbbf4596ba5da6a470b11
Brodaigh Lynch <brodaigh@gmail.com>
2009-01-04 01:55:12 +1100

    fix for safari, oops

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,13 +1,12 @@
 module VideosHelper
   
   def embed(video)
-    "<object width=\"425\" height=\"355\">
-    <param name=\"movie\" value=\"http://www.youtube.com/v/#{video}&amp;hl=en\"/>
-    <param name=\"wmode\" value=\"opaque\"/>
-    </object>
-    <object data=\"http://www.youtube.com/v/#{video}&amp;hl=en\" 
+    "<object data=\"http://www.youtube.com/v/#{video}&amp;hl=en\" 
     type=\"application/x-shockwave-flash\" width=\"425\" height=\"355\">
-    <param name=\"wmode\" value=\"opaque\"/> 
+    <param name=\"movie\" value=\"http://www.youtube.com/v/#{video}&amp;hl=en\"/>
+    <param name=\"wmode\" value=\"transparent\"/>
+    <param name=\"allowFullScreen\" value=\"true\"></param>
     </object>"
   end
 end
+
